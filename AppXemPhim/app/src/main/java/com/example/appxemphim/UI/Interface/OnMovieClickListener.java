1e405208a8bc164fe42811f48bdbaebc4fbd34f3
vucongchien <138843999+vucongchien@users.noreply.github.com>
2025-05-07 22:46:55 +0700

    Delete AppXemPhim directory

--- AppXemPhim/app/src/main/java/com/example/appxemphim/UI/Interface/OnMovieClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.appxemphim.UI.Interface;
-
-import com.example.appxemphim.Model.MovieOverviewModel;
-
-public interface OnMovieClickListener {
-    void OnMovieClick(String  movieId);
-}
